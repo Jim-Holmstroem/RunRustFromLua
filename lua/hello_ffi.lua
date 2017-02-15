103eecfc8d3cca9b06d440adcf2eb07fbaafeec0
Johan Gardell <johan@gardell.me>
2017-02-15 16:45:37 +0100

    malloc'd const char * from Rust with explicit unmanaged lifecycle.
    Lua receives ownership of a const char *,
    free'd using stdlib free.

--- lua/hello_ffi.lua
+++ lua/hello_ffi.lua
@@ -4,9 +4,20 @@ ffi.cdef[[
 void hello();
 int64_t add(int64_t, int64_t);
 int64_t length(const char *);
+char *duplicate(int64_t, const char *msg);
+// stdlib
+void free(void *);
 ]]
 local rust = ffi.load("rust")
+
+function duplicate(count, msg)
+    local c_str = rust.duplicate(count, msg)
+    local str = ffi.string(c_str)
+    ffi.C.free(c_str)
+    return str
+end
 
 rust.hello()
 print(rust.add(13, 37))
 print(rust.length("slimjim"))
+print(duplicate(3, "bork!"))
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,9 @@
 extern crate libc;
 
 use libc::c_char;
+use libc::c_void;
+use libc::malloc;
+use libc::memcpy;
 use std::ffi::CStr;
 
 #[no_mangle]
@@ -17,4 +20,19 @@ pub extern fn add(a: i64, b: i64) -> i64 {
 pub extern fn length(str: *const c_char) -> i64 {
     let c_str = unsafe { CStr::from_ptr(str) };
     c_str.to_bytes().len() as i64
+}
+
+#[no_mangle]
+pub extern fn duplicate(count: i64, msg: *const c_char) -> *mut c_char {
+    let len = length(msg);
+
+    unsafe {
+        let out = malloc((count * len + 1) as usize);
+
+        for i in 0..count {
+            memcpy(out.offset((i * len) as isize), msg as *mut c_void,
+                len as usize);
+        }
+        out as *mut c_char
+    }
 }
