ce3f0a74be3a06aa3d64559c2e407b36dcede6a5
Jim-Holmstroem <jim.holmstroem@gmail.com>
2017-02-15 12:35:19 +0000

    First try on strings

--- lua/hello_ffi.lua
+++ lua/hello_ffi.lua
@@ -3,8 +3,10 @@ local ffi = require("ffi")
 ffi.cdef[[
 void hello();
 int64_t add(int64_t, int64_t);
+int64_t length(char *);
 ]]
 local rust = ffi.load("rust/target/release/librust.so")
 
 rust.hello()
 print(rust.add(13, 37))
+print(rust.length("slimjim"))
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,3 +7,8 @@ pub extern fn hello() {
 pub extern fn add(a: i64, b: i64) -> i64 {
     a + b
 }
+
+#[no_mangle]
+pub extern fn length(str: String) -> i64 {
+    str.len() as i64
+}
